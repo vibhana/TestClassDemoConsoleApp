b90c694393d4e5dd1a51ff33313c33261e091789
vibha <vibha.ch@gmail.com>
2021-05-23 07:52:49 -0400

    Add project files.

---
+++ TestClassDemoConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestClassDemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xs = Console.ReadLine();
+            string ys = Console.ReadLine();
+            string zs = Console.ReadLine();
+
+            int x = Convert.ToInt32(xs);
+            int y = Convert.ToInt32(ys);
+            int z = Convert.ToInt32(zs);
+
+            if (x > y)
+            {
+                if (x > z)
+                    Console.WriteLine(x);
+                else
+                    Console.WriteLine(z);
+            }
+            else
+            {
+                if (y > z)
+                    Console.WriteLine(y);
+                else
+                    Console.WriteLine(z);
+            }
+        }
+    }
+}
